9463785973d66964d09ecf4e5d966ba087788cf5
M Zubair <md.zubair143598@gmail.com>
2023-03-05 13:56:42 +0500

    final_version

---
+++ Indix.js
@@ -0,0 +1,12 @@
+// const leftUL = document.querySelectorAll("#left-ul");
+// const rightContent = document.querySelector("#right-content");
+
+// leftUL.forEach((ul) => {
+//   ul.addEventListener("click", (event) => {
+//     // prevent the default behavior of the click event
+//     event.preventDefault();
+
+//     // update the contents of the right-content element
+//     rightContent.innerHTML = ul.innerHTML;
+//   });
+// });
